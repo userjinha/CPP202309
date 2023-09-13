34612caffec42b2ee3416d4b91cfee458e7e269e
userjinha <a01038627924@gmail.com>
2023-09-13 22:46:18 +0900

    first commit

---
+++ practice/week2/cout2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	char character = 'C';
+	int interger = 100;
+	double precision = 3.14159;
+	bool is_true = true;
+	string word = "Hello World";
+
+	cout << "char : " << character << endl;
+	cout << "int : " << interger << endl;
+	cout << "double : " << precision << endl;
+	cout << "bool : " << is_true << endl;
+	cout << "string : " << word << endl;
+
+	return 0;
+}
---
+++ practice/week2/dataSize.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "char 크기 : " << sizeof('p') << endl;
+	cout << "int 크기 : " << sizeof(10) << endl;
+	cout << "double 크기 : " << sizeof(10.0) << endl;
+
+	return 0;
+}
