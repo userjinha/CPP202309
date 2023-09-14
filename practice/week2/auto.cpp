4f520993b93d846c81e18d48e9879d8169b64192
userjinha <143943345+userjinha@users.noreply.github.com>
2023-09-14 22:32:16 +0900

    auto.cpp

--- practice/week2/auto.cpp
+++ practice/week2/auto.cpp
@@ -4,10 +4,14 @@ using namespace std;
 auto add(int x, int y) {
 	return x + y;
 }
+// auto: 변수/표현식의 데이터 형식을 컴파일러가 자동으로 추천, 'add' 함수 선언 ->
+// 두개의 정수를 입력으로 받아서 그들의 합을 반환
 
 int main()
 {
 	auto sum = add(5, 6);
+	// add 함수를 호출, 그 결과를 sum이라는 변수에 저장
 	cout << sum;
+	// 변수 sum의 값 출력
 	return 0;
 }
