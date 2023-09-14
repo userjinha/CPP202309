b8e98b2ddc05474f18ca8767904160edf2c76126
userjinha <143943345+userjinha@users.noreply.github.com>
2023-09-14 20:57:22 +0900

    문자열형.cpp

--- practice/week2/문자열형.cpp
+++ practice/week2/문자열형.cpp
@@ -1,10 +1,14 @@
-#include <iostream>
-#include <string>
-using namespace std;
+#include <iostream> 
+// iostream 헤더파일을 추가
+#include <string> 
+// string 헤더파일을 추가
+using namespace std; 
+// 이름 공간 설정
 
 int main()
+// 함수 선언
 {
-	string s1 = "Good";
+	string s1 = "Good"; // 
 		string s2 = "Mornig";
 		string s3 = s1 + " " + s2 + "!";
 		cout << s3 << endl;
