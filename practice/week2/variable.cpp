73992ce267004438fcba7ce3ec37696ea0a4f95e
userjinha <143943345+userjinha@users.noreply.github.com>
2023-09-14 23:31:52 +0900

    variable.cpp

--- practice/week2/variable.cpp
+++ practice/week2/variable.cpp
@@ -6,9 +6,12 @@ int main()
 {
 	int one = 1;
 	int two = 2;
+	// 정수 변수 one과 two를 선언하고 1과2로 초기화
 	int sum = one + two; 
+	// one, two 변수에 있는 값을 더하여 sum에 저장한다
 
 	cout << "1 + 2 = " << sum << endl;
+	// 1 + 2 = 를 출력한 후, sum 변수에 저장된 값을 출력
 
 	return 0;
 }
