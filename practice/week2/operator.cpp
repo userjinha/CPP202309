813f57bcf0dd65fc7a50acbdab4f55747e9961dd
userjinha <143943345+userjinha@users.noreply.github.com>
2023-09-15 00:12:04 +0900

    operator.cpp

--- practice/week2/operator.cpp
+++ practice/week2/operator.cpp
@@ -7,9 +7,11 @@ int main()
 	int x = 1;
 	int y = -1;
 	int z = -2;
+	// 변수 선언 및 각 값 할당
 
 	cout << "1 + (-1) = " << x + y << endl;
 	cout << " -1 + -2 " << y + z << endl;
+	// 문자열 출력, y+z의 결과를 출력
 
 	return 0;
 }
