f4279d0263886b73554c1ca25df89cde76661948
userjinha <143943345+userjinha@users.noreply.github.com>
2023-09-14 21:17:48 +0900

    논리형.cpp

--- practice/week2/논리형.cpp
+++ practice/week2/논리형.cpp
@@ -1,9 +1,15 @@
-#include <iostream>
+#include <iostream> 
+// <iostrem> 헤더파일을 포함, 표준 입력 및 출력 스트림을 사용하기 위해 필요
 using namespace std;
-
+// 이름 공간 설정, std네임스페이스의 멤버를 사용할 수 있도록 줌 , 예: cout
 int main()
+// 함수 선언
+
 {
-	bool b;
+	bool b; 
+	// bool 타입의 변수 b를 선언, bool 변수는 논리형으로 true 또는 flase만의 가질 수 있음
 	b = true;
+	// 변수 b에 true 값을 가질 수 있는 변수
 	return 0;
+	// int 0 반환, 현재 실행 중인 함수를 끝내려고 씀
 }
