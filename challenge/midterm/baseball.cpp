ce22b691f847eb52f084c153b8dc63537d0d53eb
userjinha <143943345+userjinha@users.noreply.github.com>
2023-11-05 21:14:24 +0900

    midterm 복습

---
+++ challenge/midterm/baseball.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool checkNumber(int randomNum);
+bool checkNumber2(int userNumber);
+int main() {
+	int randomNum; // 맞춰야 하는 3자리수의 숫자 저장 변수
+	
+
+	while (1) {
+		randomNum = rand() % 900 + 100; // 랜덤한 3자리수 생성
+
+		bool same = false; // 모든 수가 다를 경우 true를 갖는 변수
+
+		// TODO 1: 3자리 숫자의 자릿수를 추출하고, 모든 수가 다른지 체크하는 코드블록 작성
+		if (checkNumber(randomNum)==true) {
+			break;
+		}
+		else {
+			continue;
+		}
+	}
+
+	int turn = 0;
+	while (1) {
+		cout << turn + 1 << "번째 시도입니다. " << endl;
+
+		int userNumber; // 사용자가 입력한 세자리수 저장 변수
+		
+
+
+		// 사용자에게 세자리 수를 입력받는 코드 블록
+		while (1) {
+			cout << "세자리 수를 입력해주세요: ";
+			cin >> userNumber;
+
+			bool same = false; // 모든 수가 다를 경우 true를 갖는 변수
+			// TODO 1: 3자리 숫자의 자릿수를 추출하고, 모든 수가 다른지 체크하는 코드블록 작성
+			if (checkNumber2(userNumber) == true) {
+				break;
+		    }
+			else {
+				continue;
+			}
+		}
+
+		int strike = 0; // 스트라이크 갯수를 저장하는 변수
+		int ball = 0; // 볼 갯수를 저장하는 변수
+
+		// TODO 2: 정답과 추측한 숫자의 자릿수와 숫자를 비교하며 힌트를 주기 위한 코드블록 작성
+		string numStr = to_string(randomNum);
+		string numSt = to_string(userNumber);
+
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (numStr[i] == numSt[j]) {
+					if (i == j) {
+						strike++;
+					}
+					else {
+						ball++;
+					}
+				}
+			}
+		}
+
+
+
+		cout << userNumber << "의 결과 : " << strike << " 스트라이크, " << ball << "볼 입니다." << endl;
+
+		if (strike == 3) {
+
+			cout << "정답을 맞췄습니다. 축하합니다.";
+			break;
+		}
+
+		turn += 1;
+	}
+
+	return 0;
+}
+
+bool checkNumber(int randomNum) {
+	string numStr = to_string(randomNum);
+	bool same = false;
+	if (numStr[0] == numStr[1] || numStr[1] == numStr[2] || numStr[2] == numStr[0]) {
+		return false;
+	}
+	else {
+		same = true;
+	}
+	return same;
+}
+
+bool checkNumber2(int userNumber) {
+	bool same = false; // 모든 수가 다를 경우 true를 갖는 변수
+	// TODO 1: 3자리 숫자의 자릿수를 추출하고, 모든 수가 다른지 체크하는 코드블록 작성
+	string numSt = to_string(userNumber);
+	if (numSt[0] == numSt[1] || numSt[1] == numSt[2] || numSt[3] == numSt[0]) {
+		cout << "입력한 숫자에 중복된 숫자가 있습니다. 다시 입력하세요";
+		return false;
+	}
+
+	if (to_string(userNumber).length() != 3) {
+		cout << "입력된 숫자가 3자리 수가 아닙니다. 다시 입력하세요." << endl;
+		return false;
+	}
+	else {
+		return true;
+	}
+}
--- project/week6/tic_tac_toe.cpp
+++ project/week6/tic_tac_toe.cpp
@@ -20,7 +20,7 @@ int main() {
 	char currentUser = 'X'; // 현재 유저의 돌을 저장하기 위한 문자 변수
 
 	while (true) {
-	//1. 누구 차례인지 출력
+		//1. 누구 차례인지 출력
 		switch (k % 2) {
 		case 0:
 			cout << k % 2 + 1 << "번 유저(X)의 차례입니다 ->";
@@ -31,11 +31,11 @@ int main() {
 			currentUser = '0';
 			break; // K가 홀수이면(나머지가1), 0플레이어의 차례
 		}
-	// 2. 좌표 입력받기
+		// 2. 좌표 입력받기
 		cout << "(x, y)좌표를 입력하세요: ";
 		cin >> x >> y; // 문장을 출력하고 입력받은 값을 변수에 저장
 
-	// 3. 입력받은 좌표의 유효성 체크
+		// 3. 입력받은 좌표의 유효성 체크
 		if (x >= numCell || y >= numCell) {
 			cout << x << ", " << y << ": ";
 			cout << "x와 y둘 중 하나가 칸을 벗어납니다." << endl;
@@ -45,12 +45,12 @@ int main() {
 			cout << x << ", " << y << ": 이미 돌이 차있습니다." << endl;
 			continue; // x,y의 좌표가 공백과 다르면 이 문장이 출력되고 계속됨
 		}
-	// 4. 입력받은 좌표에 현재 유저의 돌 놓기
-		board[x][y] = currentUser; 
+		// 4. 입력받은 좌표에 현재 유저의 돌 놓기
+		board[x][y] = currentUser;
 
-	// 5. 현재 보드 판 출력
+		// 5. 현재 보드 판 출력
 		for (int i = 0; i < numCell; i++) {
-			cout << "---|---|---" << endl; 
+			cout << "---|---|---" << endl;
 			for (int j = 0; j < numCell; j++) {
 				cout << board[i][j];
 				if (j == numCell - 1) {
@@ -59,12 +59,12 @@ int main() {
 				cout << "  |"; //문장 출력
 			}
 			cout << endl;// 줄바꿈
-	
+
 			cout << "---|---|---" << endl; // 문장 출력
 			k++; // 누구차례인지 증감
 
 		}
-		
+
 		// 6. 모든 칸 다 찼는지 체크하기
 		int checked = 0;
 		for (int i = 0; i < numCell; i++) {
@@ -73,43 +73,46 @@ int main() {
 					checked++;
 				}
 			}
-			}
-				if (checked == 0) {
+		}
+		if (checked == 0) {
 			cout << "모든 칸이 다 찼습니다. 종료합니다" << endl;
 			break;
-		
 
-		// 7. 승자 체크하기
-		bool isWin = false;
 
-		// 7.1. 가로/세로돌 체크하기
-		for (int i = 0; i < numCell; i++) {
-			if (board[i][0] == currentUser && board[i][1] == currentUser && board[i][2] == currentUser) {
-				cout << "가로에 모두 돌이 놓였습니다!: "; // 가로에 입력받은 값에 저장된 값과 동일하면 이 문장이 출력
-				isWin = true; // true로 초기화
+			// 7. 승자 체크하기
+			bool isWin = false;
+
+			// 7.1. 가로/세로돌 체크하기
+			for (int i = 0; i < numCell; i++) {
+				if (board[i][0] == currentUser && board[i][1] == currentUser && board[i][2] == currentUser) {
+					cout << "가로에 모두 돌이 놓였습니다!: "; // 가로에 입력받은 값에 저장된 값과 동일하면 이 문장이 출력
+					isWin = true; // true로 초기화
+				}
+				if (board[0][i] == currentUser && board[1][i] == currentUser && board[2][i] == currentUser) {
+					cout << "세로에 모두 돌이 놓였습니다!: ";
+					isWin = true;
+				}
 			}
-			if (board[0][i] == currentUser && board[1][i] == currentUser && board[2][i] == currentUser) {
-				cout << "세로에 모두 돌이 놓였습니다!: ";
-				isWin = true;
-			}
-		}
 
-		// 7.2 대각선돌 체크하기
-		if (board[0][0] == currentUser && board[1][1] == currentUser && board[2][2] == currnetUser) {
-			cout << "왼쪽 위에서 오른쪽 아래 대각선으로 모두 돌이 놓였습니다!";
-			isWin = true; // 대각선에 입력받은 값에 저장된 값과 동일하면 이 문장이 출력
-		}
-		if (board[0][2] == currentUser && board[1][1] == currentUser && board[2][0] == currentUser) {
+			// 7.2 대각선돌 체크하기
+			if (board[0][0] == currentUser && board[1][1] == currentUser && board[2][2] == currentUser) {
+				cout << "왼쪽 위에서 오른쪽 아래 대각선으로 모두 돌이 놓였습니다!";
+				isWin = true; // 대각선에 입력받은 값에 저장된 값과 동일하면 이 문장이 출력
+			}
+			if (board[0][2] == currentUser && board[1][1] == currentUser && board[2][0] == currentUser) {
 				cout << "오른쪽 위에서 왼쪽 아래 대각선으로 모두 돌이 놓였습니다!";
 				isWin = true;
-			} 
-		if (isWin == true) {
-				cout << "k % 2 +1" << "번 유저(" << currentUser << ")의 승리입니다!"<< endl;
+			}
+			if (isWin == true) {
+				cout << "k % 2 +1" << "번 유저(" << currentUser << ")의 승리입니다!" << endl;
 				cout << "종료합니다" << endl;
 				break; // 만약 ture이면 이문장이 출력되며 종료된다.
 			}
-		
+
+		}
 	}
 }
+
+
 
 
